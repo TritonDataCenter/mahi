96bb22ea980f9cb8268053e8b29c0bfd26575574
Fred Kuo <fred.kuo@joyent.com>
2014-08-25 22:53:30 +0000

    TOOLS-607 I, for one, welcome my new open source license

--- bin/clearcache.sh
+++ bin/clearcache.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
 
 dbnum=$(json -f etc/mahi2.json redis.db || 0)
 svcadm disable mahi-replicator
